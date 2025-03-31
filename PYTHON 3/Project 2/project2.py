92ff0354d849f534acf93701d6951ff33afce378
jdarchila <juandavidarchilaleon@gmail.com>
2025-03-31 14:35:23 -0600

    saving

--- PYTHON 3/Project 2/project2.py
+++ PYTHON 3/Project 2/project2.py
@@ -1,57 +1,64 @@
-try:
-  weight = float(input("What's the weight of your package? "))
-except ValueError:
-    print("Error: Please enter a valid number for the weight.")
-    exit(1)  # Exits the program if the input is invalid
-print("")
+while True:
+  while True:
+    try:
+      weight = float(input("What's the weight of your package? "))
+      break # Break out of the loop if the input is valid
+    except ValueError:
+        print("Error: Please enter a valid number for the weight.")
+  print("")
 
 
-cost_ground_shipping = " "
+  cost_ground_shipping = " "
 
-#Cost Ground Shipping 
+  #Cost Ground Shipping 
 
-if weight >= 10:
-  cost_ground_shipping = (weight * 4.75) + 20 
-elif weight >= 6:
-  cost_ground_shipping = (weight * 4) + 20
-elif weight >= 2:
-  cost_ground_shipping = (weight * 3) + 20
-elif weight < 2:
-  cost_ground_shipping = (weight * 1.50) + 20
+  if weight >= 10:
+    cost_ground_shipping = (weight * 4.75) + 20 
+  elif weight >= 6:
+    cost_ground_shipping = (weight * 4) + 20
+  elif weight >= 2:
+    cost_ground_shipping = (weight * 3) + 20
+  elif weight < 2:
+    cost_ground_shipping = (weight * 1.50) + 20
 
-print ("Your total for Ground Shipping is $", cost_ground_shipping)
+  print ("Your total for Ground Shipping is $", cost_ground_shipping)
 
-#Cost Ground Shipping Premium
+  #Cost Ground Shipping Premium
 
-cost_ground_shipping_premium = 125
+  cost_ground_shipping_premium = 125
 
-print ("Your total for Ground Premium is $", cost_ground_shipping_premium)
+  print ("Your total for Ground Premium is $", cost_ground_shipping_premium)
 
-#Cost Drone Shipping
+  #Cost Drone Shipping
 
-if weight >= 10:
-  cost_drone_shipping = (weight * 14.25) 
-elif weight >= 6:
-  cost_drone_shipping = (weight * 12) 
-elif weight >= 2:
-  cost_drone_shipping = (weight * 9) 
-elif weight < 2:
-  cost_drone_shipping = (weight * 4.50)
+  if weight >= 10:
+    cost_drone_shipping = (weight * 14.25) 
+  elif weight >= 6:
+    cost_drone_shipping = (weight * 12) 
+  elif weight >= 2:
+    cost_drone_shipping = (weight * 9) 
+  elif weight < 2:
+    cost_drone_shipping = (weight * 4.50)
 
-print ("Your total for Drone Shipping is $", cost_drone_shipping)
+  print ("Your total for Drone Shipping is $", cost_drone_shipping)
 
-print("")
+  print("")
 
-# Determine the cheapest shipping option using min()
-best_cost = min(cost_ground_shipping, cost_ground_shipping_premium, cost_drone_shipping)
+  # Determine the cheapest shipping option using min()
+  best_cost = min(cost_ground_shipping, cost_ground_shipping_premium, cost_drone_shipping)
 
-if best_cost == cost_ground_shipping:
-    print("The best option is Ground Shipping, it costs $", cost_ground_shipping)
-elif best_cost == cost_ground_shipping_premium:
-    print("The best option is Ground Shipping Premium, it costs $", cost_ground_shipping_premium)
-else:
-    print("The best option is Drone Shipping, it costs $", cost_drone_shipping)
+  if best_cost == cost_ground_shipping:
+      print("The best option is Ground Shipping, it costs $", cost_ground_shipping)
+  elif best_cost == cost_ground_shipping_premium:
+      print("The best option is Ground Shipping Premium, it costs $", cost_ground_shipping_premium)
+  else:
+      print("The best option is Drone Shipping, it costs $", cost_drone_shipping)
 
-print("")
+  print("")
 
+  # Ask if the user wants to calculate again
+  again = input("Would you like to calculate shipping for another package? (y/n): ")
+  if again.lower() != 'y':
+    print("Goodbye!")
+    break
 
