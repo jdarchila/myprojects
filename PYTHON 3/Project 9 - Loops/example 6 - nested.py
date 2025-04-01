e3e04c1ec81c55c375d097d782c29fd70d94987f
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-01 12:30:59 -0600

    saving

---
+++ PYTHON 3/Project 9 - Loops/example 6 - nested.py
@@ -0,0 +1,13 @@
+project_teams = [["Ava", "Samantha", "James"], ["Lucille", "Zed"], ["Edgar", "Gabriel"]]
+
+for team in project_teams:
+  print(team)
+
+print("==============================")
+
+for team in project_teams:
+  print (team)
+  for student in team:
+    print(student)
+  
+
