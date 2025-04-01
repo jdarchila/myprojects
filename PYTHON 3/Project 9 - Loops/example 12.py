8544bcd048646012de377e8def7469dba03da49d
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-01 13:46:36 -0600

    saving

---
+++ PYTHON 3/Project 9 - Loops/example 12.py
@@ -0,0 +1,15 @@
+
+single_digits = range(10)
+
+squares = [digits**2 for digits in single_digits]
+
+print(squares)
+
+print("")
+
+cubes = [digits**3 for digits in single_digits]
+
+print(cubes)
+
+print("")
+
---
+++ PYTHON 3/Project 9 - Loops/example 13.py
@@ -0,0 +1,19 @@
+print("")
+
+single_digits = range(10)
+
+square = []
+
+for digits in single_digits:
+  square = digits**2
+  print(square)
+
+print("")
+
+cube = []
+
+for digits in single_digits:
+  cube = digits**3
+  print(cube)
+
+print("")
