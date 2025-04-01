8e44052890e9267fcbd09c55f2a7ed0a7b82cdbc
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-01 11:51:53 -0600

    saving

---
+++ PYTHON 3/Project 9 - Loop control break/loopcontrolbreak.py
@@ -0,0 +1,10 @@
+items_on_sale = ["blue shirt", "striped socks", "knit dress", "red headband", "dinosaur onesie"]
+
+print("Checking the sale list!")
+
+for item in items_on_sale:
+  print(item)
+  if item == "knit dress":
+    break
+
+print("End of search!")
