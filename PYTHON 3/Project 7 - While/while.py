0a285645afd67aeed4c55cdc10b12b0ad8a941e7
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-01 11:42:43 -0600

    saving

---
+++ PYTHON 3/Project 7 - While/while.py
@@ -0,0 +1,10 @@
+count = 0 
+
+
+print("--------------------------------------")
+
+
+while count < 10 :
+  print(count)
+  count+=2
+
---
+++ PYTHON 3/Projects 8 - While loops lists/whileloopslists.py
@@ -0,0 +1,12 @@
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+#Your code below: 
+
+length = len(python_topics)
+index = 0 
+
+while index < length:                   #Checks until when to stop
+  print(python_topics[index])           #Prints item in the list based on it's position "[]" helps to determine that position?
+  print(index)                          #Prints the position of the item. 
+  index += 1                            #Makes the loop to work properly because it makes it move through the list kinda. 
+
