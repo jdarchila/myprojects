f1b1baec56330a36c2d21766499b3a1e23ec6584
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-02 16:37:43 -0600

    saving

---
+++ PYTHON 3/example 20.py
@@ -0,0 +1,11 @@
+num1 = 6
+num2 = 4
+
+# Write your if statement here
+if num1 + num2 != 10:
+  not_ten = True
+else:
+  not_ten = False
+
+# Uncomment the below lines to show the result
+print("Is the sum of the numbers not equal to 10? " + str(not_ten))
