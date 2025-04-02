2ff1f0d8d636078c543314ac74b8859a14553110
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-02 00:04:28 -0600

    saving

---
+++ PYTHON 3/Project 14/example15.py
@@ -0,0 +1,13 @@
+# Program to add two numbers
+
+# Ask the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Ask the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {result}.")
