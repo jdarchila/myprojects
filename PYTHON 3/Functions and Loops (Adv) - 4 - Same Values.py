0dc8bacf4a3c4a58dab275efd91f3066d935ce62
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-03 12:03:59 -0600

    saving

--- PYTHON 3/Functions and Loops (Adv) - 4 - Same Values.py
+++ PYTHON 3/Functions and Loops (Adv) - 4 - Same Values.py
@@ -10,3 +10,23 @@
 
 #The function should return a list of the indices where the values were equal in lst1 and lst2.
 
+def same_values(lst1, lst2):
+    
+    matching_indices = []
+
+    # Loop through the indices of the lists
+    for i in range(len(lst1)):
+        # Check if the values at the current index are equal
+        if lst1[i] == lst2[i]:
+            # If they are equal, append the index to the list
+            matching_indices.append(i)
+
+    # Return the list of matching indices
+    return matching_indices
+
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])) # Expected output: [0, 3]
+print(same_values([1, 2, 3], [4, 5, 6])) # Expected output: []
+print(same_values([1, 2, 3], [1, 2, 3])) # Expected output: [0, 1, 2]
+print(same_values([1, 2, 3], [3, 2, 1])) # Expected output: []
+print(same_values([1, 2, 3], [1, 2, 4])) # Expected output: [0, 1]
+print(same_values([1, 2, 3], [4, 5, 6])) # Expected output: []  
