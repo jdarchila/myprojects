2e2fd149e6061889fadb9e98ee64d3c0a32c1691
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-03 10:28:39 -0600

    saving

--- PYTHON 3/Functions and Loops (Adv) - 3 - Max Num.py
+++ PYTHON 3/Functions and Loops (Adv) - 3 - Max Num.py
@@ -10,3 +10,39 @@
 
 #The function should return the largest number in nums
 
+#One way  
+def max_num(nums):
+  max_value = [0]
+  for num in nums:
+    if num > max_value[0]:
+      max_value[0]= num 
+  return max_value 
+
+print(max_num([50, -10, 0, 75, 20])) # 75
+
+
+print("")
+
+#Another way
+
+def max_num2(nums2):
+  for num2 in nums2:
+    if num2 == max(nums2):
+      return num2 
+  return num2
+
+print (max_num2([50, -10, 0, 75, 20])) # 75
+
+print ("")
+
+#Another way
+
+def max_num3(nums3):
+  maximum = nums3[0]
+  for number in nums3:
+    if number > maximum:
+      maximum = number
+  return maximum
+  
+
+print(max_num3([50, -10, 0, 75, 20])) # 75
