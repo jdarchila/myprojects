b957fc004685fba4f78709014943bb76b207483d
jdarchila <juandavidarchilaleon@gmail.com>
2025-04-04 14:41:14 -0600

    saving

---
+++ PYTHON 3/Strings Methods - 1 - Joining Strings.py
@@ -0,0 +1,8 @@
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+
+print(reapers_line_one_words)
+
+reapers_line_one = ' '.join(reapers_line_one_words)
+
+print(reapers_line_one)
+
---
+++ PYTHON 3/Strings Methods - 3 - Strip.py
@@ -0,0 +1,10 @@
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+print(love_maybe_lines)
+
+love_maybe_lines_stripped = []
+
+for line in love_maybe_lines:
+  love_maybe_lines_stripped.append(line.strip()) 
+
+print(love_maybe_lines_stripped)
